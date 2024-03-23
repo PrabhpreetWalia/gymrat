afad5af03e59bb133561f4447505be7ba11d8205
Prabhpreet Walia <prabhpreet.business@gmail.com>
2024-03-23 12:14:06 +0530

    Skeleton loading added

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -4,48 +4,50 @@ import PrimaryButton from "../PrimaryButton/PrimaryButton";
 import { useState, useEffect } from "react";
 import { Link, generatePath, useParams } from "react-router-dom";
 
-function News({ margin = "10vh 0 0 0", count=-1}) {
-
-  window.scrollTo({top: 0, behavior: 'smooth'})
+function News({ margin = "10vh 0 0 0", count = -1 }) {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 
   const [news, setNews] = useState([]);
-  const [pagination, setPagination] = useState()
+  const [pagination, setPagination] = useState();
 
-  const {page} = useParams()
+  const { page } = useParams();
 
-  let API_URL = "https://gymrat-api-five.vercel.app"
+  let API_URL = "https://gymrat-api-five.vercel.app";
 
   useEffect(() => {
-
-    try{
-
-      if (count != -1) return
+    try {
+      if (count !== -1) return;
 
       fetch(`${API_URL}/count-news`)
-        .then(res => res.json())
+        .then((res) => res.json())
         .then((data) => {
-
-          const pageCount = data.pageCount
+          const pageCount = data.pageCount;
           let paginationHTML = [];
 
-          for(let i =1; i<= pageCount; i++){
-            paginationHTML.push(<Link to={generatePath("/blog/:page", {page:i})}><button className="pagination--button" disabled={page==i}>{i}</button></Link>)
+          for (let i = 1; i <= pageCount; i++) {
+            paginationHTML.push(
+              <Link key={i} to={generatePath("/blog/:page", { page: i })}>
+                <button
+                  className="pagination--button"
+                  disabled={page === String(i)}
+                >
+                  {i}
+                </button>
+              </Link>
+            );
           }
-          setPagination(paginationHTML)
-
-        })
+          setPagination(paginationHTML);
+        });
+    } catch (e) {
+      console.log(e);
     }
-    catch(e){
-      console.log(e)
-    }
-
-  }, [page])
+  }, [page, API_URL, count]);
 
   useEffect(()=> {
 
     try {
 
-      if (count != -1){
+      if (count !== -1){
 
         fetch(`${API_URL}/news?num=${count}`)
           .then(data => data.json())
@@ -61,33 +63,53 @@ function News({ margin = "10vh 0 0 0", count=-1}) {
           setNews(data);
         });
       }
-      
+
     } catch (e) {
       console.log(e);
     }
-  }, [count, page]);
+  }, [API_URL, count, page]);
+
+  function newsPropogate() {
+    return news.map((item, index) => {
+      return (
+        <div key={index} className="news">
+          <div className="news--date">{item.date}</div>
+          <h1>{item.headline}</h1>
+          <h2>{item.content}</h2>
+          <PrimaryButton value="Read More" isBlack={true} href="/blog/1" />
+        </div>
+      );
+    });
+  }
+
+  function skeletonLoading() {
+
+    const skeleltonItem = []
+
+    for(var i=0; i< 6; i++){
+      skeleltonItem.push(
+        <div key={i} className="news--skeleton">
+          <div className="news--skeleton-bar"></div>
+          <div className="news--skeleton-bar"></div>
+          <div className="news--skeleton-bar"></div>
+          <div className="news--skeleton-bar"></div>
+        </div>
+      )
+    }
+
+          
+          return ( <>{skeleltonItem}</> );
+  }
 
   return (
     <>
       <div className="news--heading" style={{ margin: margin }}>
         <span className="main--heading">LATEST NEWS</span>
-        
       </div>
       <div className="news--container">
-        {news.map((item, index) => {
-          return (
-            <div className="news" key={index}>
-              <div className="news--date">{item.date}</div>
-              <h1>{item.headline}</h1>
-              <h2>{item.content}</h2>
-              <PrimaryButton value="Read More" isBlack={true} href="/blog/1"/>
-            </div>
-          );
-        })}
+        {news.length === 0 ? skeletonLoading() : newsPropogate()}
       </div>
-      <div className="pagination--container">
-        {pagination}
-      </div>
+      <div className="pagination--container">{pagination}</div>
     </>
   );
 }
